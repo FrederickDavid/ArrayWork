f75f97d6795c6a9c8edecf44a263a8656cb2e967
FrederickDavid <muomaifefrederick61@gmail.com>
2022-06-01 12:53:52 -0700

    Commit

---
+++ array.js
@@ -0,0 +1,127 @@
+let output = document.getElementById("array1")
+
+// let Names = ["Confidence", "Divine", "Miracle"]
+
+// for (let i = 0; i < Names.length; i++) {
+//     if(Names[i]){
+//         output.innerHTML = Names
+//     }
+// }
+
+// let output1 = document.getElementById("array1")
+
+// for (let i = Names.length-1; i>=0; i--){
+//  output1.innerHTML += Names[i] + "<br/>"
+// }
+
+// let m = 0
+// while(Names[m]){
+//     output.innerHTML += Names[m] + "<br/>"
+//     m++;
+// }
+
+// let show = document.getElementById("array")
+// let user = prompt("How many banana did you eat today?");
+// let kg = 89;
+// let total = 0;
+
+// for(let x=1; x<=user; x++){
+//     if(x<=1){
+//         let me = "bannana"
+//         total += kg
+//         output.innerHTML += "You ate" + x + me +  "total calories is" + total + "</br>"
+//     }else{
+//         let me = "bannanas"
+//         total += kg
+//     output.innerHTML += "You ate" + x + me +  "total calories is" + total + "</br>"
+//     }
+    
+// }
+
+// USING A NESTED LOOP
+
+// let html = "<table>"
+
+// for(let row = 0; row<5; row++){
+//     html += "<tr>"
+//     html += "<td>"
+//     html += "firstName"
+//     html += "</td>"
+//     html += "</td>"
+
+// }
+
+// // html += "</table>"
+
+
+let browser = document.getElementById("ass")
+
+// for(let row=0; row<5; row++){
+//     html += "<tr>"
+//     for (let col=0; col<6; col++){
+//         html += "<td> cell =" +col + "</td>"
+//     }
+//     html += "</tr>"
+// }
+
+// html += "</table>"
+
+// browser.innerHTML = html
+
+// for (let x= 0; x<5; x++){
+//     for(let i = 0; i<5; i++){
+//         console.log(x,i)
+//     }
+// }
+
+// for(let x=10; x>0; x--){
+//     console.log(x)
+// }
+
+
+// for(a=0; a<1; a++){
+//     console.log(a)
+//     for(b=1; b<2; b++){
+//         console.log(b,a)
+//     }
+//    for(c=2; c<3; c++){
+//        console.log(c,b,a)
+//    }
+// }\
+
+
+
+
+// for(let x=0; x<5; x++){
+//     for(let j=x; j>=0; j--){
+//         browser.append(j + " ")
+//     }let br = document.createElement("br")
+//     browser.appendChild(br)
+// }
+// for(let x=5; x>=0; x--){
+//     for(let j=x; j>=0; j--){
+//         output.append(j + "")
+//     }let br = document.createElement("br")
+//     output.appendChild(br)
+// }
+// let browser1 = document.getElementById("array1")
+
+// // for(x=0; x<=1000; x++)
+// // browser.innerHTML += x + "<br>" + "<br>"
+
+// let total = 0
+
+// for(x=0; x<=1000; x++){
+//     if(x % 5){
+//         browser.innerHTML += ""
+//     }else{
+//         browser.innerHTML +=   x +  "<br>"
+
+//         total += x        
+//     }
+   
+
+// }
+
+// browser1.innerHTML = total
+
---
+++ reading.js
@@ -0,0 +1,22 @@
+// let total = 0, count = 1;
+// while(count<=10){
+//   total += count
+//   count += 1
+// }
+// console.log(total)
+
+// console.log(Math.sum(range(1, 10)))
+
+// function factorial(n){
+//     if(n==0){
+//         return 1
+//     }else{
+//        return factorial(n -1) * n
+//     }
+// }
+
+// console.log(factorial(7)) mul = a -1 * a
+// mul = 7 * 8 mul = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 
+
+console.log("good" < NaN)
+
